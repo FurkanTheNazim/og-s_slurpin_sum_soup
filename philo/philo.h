a70eee0c1b725b2367db342b22d5d32719dce14c
FurkanTheNazim <moussafurkan0@gmail.com>
2026-06-05 00:14:40 +0300

    init: project skeleton with Makefile and header

---
+++ philo/philo.h
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mahmmous <mahmmous@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/05 00:00:00 by mahmmous          #+#    #+#             */
+/*   Updated: 2026/06/05 00:00:00 by mahmmous         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <sys/time.h>
+
+typedef struct s_philo
+{
+	int				id;
+	int				meals_eaten;
+	long long		last_meal_time;
+	pthread_t		thread;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
+	struct s_data	*data;
+}	t_philo;
+
+typedef struct s_data
+{
+	int				num_philos;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				must_eat_count;
+	int				sim_stop;
+	long long		start_time;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	print_mutex;
+	pthread_mutex_t	meal_mutex;
+	pthread_mutex_t	stop_mutex;
+	t_philo			*philos;
+}	t_data;
+
+#endif
